c2829be78cb190ebcbd4a7290e93dab7f764580f
Craig Day <cday@zendesk.com>
2020-12-04 10:51:11 -0800

    remove old ui dir

--- ui/src/redux/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import gameReducer from '../features/game/reducer'
-
-const rootReducer = combineReducers({
-  games: gameReducer,
-})
-
-export default rootReducer
