76aec9ac3db3ea373aa7474e371c1d596f0f0c59
Craig Day <cday@zendesk.com>
2020-12-13 12:57:16 -0800

    setup apollo with http and phoenix socket link

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,15 +1,49 @@
 import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+  split,
+} from '@apollo/client'
+import * as AbsintheSocket from '@absinthe/socket'
+import { createAbsintheSocketLink } from '@absinthe/socket-apollo-link'
+import { Socket as PhoenixSocket } from 'phoenix'
+import { hasSubscription } from '@jumpn/utils-graphql'
 import Landing from './features/landing/Landing'
 
+const httpLink = createHttpLink({
+  uri: 'https://localhost:4000/graphql',
+})
+
+const phoenixSocket = new PhoenixSocket('ws://localhost:4000/socket')
+
+const absintheSocket = AbsintheSocket.create(phoenixSocket)
+
+const websocketLink = createAbsintheSocketLink(absintheSocket)
+
+const link = split(
+  (operation) => hasSubscription(operation.query),
+  websocketLink,
+  httpLink
+)
+
+const apolloClient = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+})
+
 const App = () => (
-  <Router>
-    <Switch>
-      <Route path="/">
-        <Landing />
-      </Route>
-    </Switch>
-  </Router>
+  <ApolloProvider client={apolloClient}>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Landing />
+        </Route>
+      </Switch>
+    </Router>
+  </ApolloProvider>
 )
 
 export default App
