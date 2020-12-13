3ae398588984a2fb6669ed054501a805501dce0f
Craig Day <cday@zendesk.com>
2020-12-13 13:16:44 -0800

    bring in grid layout for page centering

--- ui/src/features/landing/Landing.js
+++ ui/src/features/landing/Landing.js
@@ -1,5 +1,22 @@
 import React from 'react'
+import { Grid, Row, Col } from '@zendeskgarden/react-grid'
 
-const Landing = () => <h1>Hello, world!</h1>
+const Landing = () => (
+  <Grid
+    style={{
+      height: '100%',
+      display: 'flex',
+      flexFlow: 'column nowrap',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <Row justifyContent="center">
+      <Col justifyContent="center">
+        <h1>Hello, world!</h1>
+      </Col>
+    </Row>
+  </Grid>
+)
 
 export default Landing
