d5958136fae4c27cfe77f15a3a343ea4fd2f208c
Craig Day <cday@zendesk.com>
2020-12-13 13:30:05 -0800

    import landing layout

--- ui/src/features/landing/Landing.js
+++ ui/src/features/landing/Landing.js
@@ -1,22 +1,57 @@
 import React from 'react'
 import { Grid, Row, Col } from '@zendeskgarden/react-grid'
+import { Button } from '@zendeskgarden/react-buttons'
 
 const Landing = () => (
-  <Grid
-    style={{
-      height: '100%',
-      display: 'flex',
-      flexFlow: 'column nowrap',
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}
-  >
-    <Row justifyContent="center">
-      <Col justifyContent="center">
-        <h1>Hello, world!</h1>
-      </Col>
-    </Row>
-  </Grid>
+  <div className="centered-grid">
+    <Grid>
+      <Row alignItems="center" justifyContent="center">
+        <Col size={3} textAlign="center">
+          <Row alignItems="center">
+            <Col isStretched>
+              <Button
+                style={{ fontSize: 'larger' }}
+                size="large"
+                isPrimary
+                isStretched
+              >
+                Create Game
+              </Button>
+            </Col>
+            <Col isStretched>
+              <Button
+                style={{ fontSize: 'larger' }}
+                size="large"
+                isPrimary
+                isStretched
+              >
+                Join Game
+              </Button>
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col isStretched>
+              <hr />
+            </Col>
+          </Row>
+          <br />
+          <Row alignItems="center">
+            <Col>
+              <Button
+                style={{ fontSize: 'larger' }}
+                size="large"
+                isStretched
+                isBasic
+              >
+                Manage Game
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Grid>
+  </div>
 )
 
 export default Landing
