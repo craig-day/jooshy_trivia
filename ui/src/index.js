44dfc1fc41e1734811e77152553a856909d2a5e7
Craig Day <cday@zendesk.com>
2020-12-21 19:22:53 -0800

    comment about strict mode

--- ui/src/index.js
+++ ui/src/index.js
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+// TODO: this would be nice to run in strict mode, but Zendesk Garden is not idempotent, so the
+// double render causes bugs: https://github.com/zendeskgarden/react-components/issues/849
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
