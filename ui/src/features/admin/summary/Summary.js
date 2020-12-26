b96896659110f1ab349082728499cd1e0d211d9c
Craig Day <cday@zendesk.com>
2020-12-26 15:28:07 -0800

    summary table instead

--- ui/src/features/admin/summary/Summary.js
+++ ui/src/features/admin/summary/Summary.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Col, Grid, Row } from '@zendeskgarden/react-grid'
-import { LG, XL, XXL } from '@zendeskgarden/react-typography'
-import { ReactComponent as NextIcon } from '@zendeskgarden/svg-icons/src/16/chevron-double-right-fill.svg'
+import { LG, Span, XL, XXL } from '@zendeskgarden/react-typography'
 import RoundTypeIcon from '../../../components/RoundTypeIcon'
-import { Tiles } from '@zendeskgarden/react-forms'
-import * as List from '../../../utils/List'
+import {
+  Body,
+  Head,
+  HeaderCell,
+  HeaderRow,
+  Table,
+  Row as TableRow,
+  Cell,
+} from '@zendeskgarden/react-tables'
 
 const SummaryRow = styled.div`
   & > div {
@@ -36,63 +42,38 @@ const StatCol = ({ title, value }) => (
   </Col>
 )
 
-const RoundTile = ({ type, name, questionCount, points }) => (
-  <Tiles
-    name={name}
-    onChange={(e) => e.preventDefault() && e.stopPropagation()}
-  >
-    <Tiles.Tile
-      value="one"
-      onClick={(e) => e.preventDefault()}
-      style={{ cursor: 'default' }}
-    >
-      <Tiles.Icon>
-        <RoundTypeIcon type={type} />
-      </Tiles.Icon>
-      <Tiles.Label>{name}</Tiles.Label>
-      <Tiles.Description>
-        {questionCount === 1 ? `1 question` : `${questionCount} questions`},{' '}
-        {points === 1 ? `1 point` : `${points} points`}
-      </Tiles.Description>
-    </Tiles.Tile>
-  </Tiles>
-)
-
-const StyledTransitionIcon = styled.div`
-  padding: ${(p) => p.theme.space.sm};
-  text-align: center;
-  flex-grow: 1;
-`
-
-const NextRound = () => (
-  <Col size={0.5}>
-    <StyledTransitionIcon>
-      <NextIcon />
-    </StyledTransitionIcon>
-  </Col>
-)
-
-const RoundFlowChart = ({ rounds }) =>
-  List.chunk(rounds, 5).map((roundChunk, i) => (
-    <React.Fragment key={`round-row-${i}`}>
-      <Row alignItems="center">
-        {roundChunk.map((round, j) => (
-          <React.Fragment key={`round-${j}`}>
-            <Col size={2}>
-              <RoundTile
-                type={round.__typename}
-                name={round.name}
-                questionCount={round.questionCount}
-                points={round.points}
-              />
-            </Col>
-            {j < roundChunk.length - 1 ? <NextRound /> : null}
-          </React.Fragment>
-        ))}
-      </Row>
-      {i < rounds.length / 5 - 1 ? <br /> : null}
-    </React.Fragment>
+const RoundRows = ({ rounds }) =>
+  rounds.map((round, i) => (
+    <TableRow key={`round-${i}`} isStriped={i % 2 === 1}>
+      <Cell isMinimum>{i + 1}.</Cell>
+      <Cell isMinimum>
+        <Span>
+          <Span.StartIcon>
+            <RoundTypeIcon type={round.__typename} />
+          </Span.StartIcon>
+          {`  ${round.name}`}
+        </Span>
+      </Cell>
+      <Cell>{round.questionCount}</Cell>
+      <Cell>{round.points}</Cell>
+    </TableRow>
   ))
+
+const RoundsTable = ({ rounds }) => (
+  <Table>
+    <Head>
+      <HeaderRow>
+        <HeaderCell isMinimum />
+        <HeaderCell>Name</HeaderCell>
+        <HeaderCell>Questions</HeaderCell>
+        <HeaderCell>Points</HeaderCell>
+      </HeaderRow>
+    </Head>
+    <Body>
+      <RoundRows rounds={rounds} />
+    </Body>
+  </Table>
+)
 
 export const Summary = ({ game }) => (
   <Grid>
@@ -121,10 +102,9 @@ export const Summary = ({ game }) => (
         <XXL>Rounds</XXL>
       </Col>
     </Row>
-    <br />
     <Row>
-      <Col>
-        <RoundFlowChart rounds={game.rounds} />
+      <Col size={6}>
+        <RoundsTable rounds={game.rounds} />
       </Col>
     </Row>
   </Grid>
