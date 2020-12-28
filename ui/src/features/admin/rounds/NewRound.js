ba617543e598e188d10769751e956427e306d600
josh-tollefson <josh.tollefson9@gmail.com>
2020-12-28 15:43:06 -0800

    split addmultiplechoiceround to own file

--- ui/src/features/admin/rounds/NewRound.js
+++ ui/src/features/admin/rounds/NewRound.js
@@ -25,6 +25,7 @@ import { ReactComponent as RecordIcon } from '@zendeskgarden/svg-icons/src/16/re
 import { ReactComponent as MusicIcon } from '@zendeskgarden/svg-icons/src/16/volume-unmuted-fill.svg'
 import { ReactComponent as SequenceIcon } from '@zendeskgarden/svg-icons/src/16/list-number-stroke.svg'
 import { ReactComponent as ImageIcon } from '@zendeskgarden/svg-icons/src/16/image-fill.svg'
+import { AddMultipleChoiceRound } from './AddMultipleChoiceRound.js'
 import * as List from '../../../utils/List'
 
 const RoundTypeIcon = ({ type }) => {
@@ -71,15 +72,6 @@ const RoundTypes = ({ types, setRound }) => (
   </Tiles>
 )
 
-const DisplayQuestion = ({ questions }) => (
-  <Row justifyContent="center">
-    <Col sm={5}>
-      <Field>
-        <Label>{questions}</Label>
-      </Field>
-    </Col>
-  </Row>
-)
 
 const AddRoundQuestions = ({
   type,
@@ -88,7 +80,6 @@ const AddRoundQuestions = ({
   answers,
   setAnswers,
 }) => {
-  const [visible, setVisible] = useState(false)
 
   switch (type) {
     case 'PickOne':
@@ -100,203 +91,19 @@ const AddRoundQuestions = ({
         </Col>
       )
     case 'MultipleChoice':
-      console.log(questions, answers)
-
-      function nextCharacter(c) {
-        return String.fromCharCode(c.charCodeAt(0) + 1)
-      }
-
-      function onAddNewQuestion(question, choices) {
-        const questionKeys = Object.keys(questions)
-        const answersKeys = Object.keys(answers)
-        let newQuestionKey
-        let newQuestions
-        let newAnswerKey
-        let newAnswers
-
-        if (questionKeys.length === 0) {
-          newQuestionKey = '1'
-        } else {
-          newQuestionKey = nextCharacter(questionKeys[questionKeys.length - 1])
-        }
-
-        newQuestions = { ...questions }
-        newQuestions[newQuestionKey] = question
-        setQuestions(newQuestions)
-
-        if (answersKeys.length === 0) {
-          newAnswerKey = '1'
-        } else {
-          newAnswerKey = nextCharacter(answersKeys[answersKeys.length - 1])
-        }
-
-        newAnswers = { ...answers }
-        newAnswers[newAnswerKey] = choices
-        setAnswers(newAnswers)
-      }
-
-      return (
-        <React.Fragment>
-          {Object.entries(questions).map(([number, question], i) => {
-            const test = Object.entries(answers[i + 1]).map(
-              ([letter, answer]) => (
-                <Row>
-                  <Col>
-                    {`${letter}. ${answer}`}
-                    <br />
-                  </Col>
-                </Row>
-              )
-            )
-            return (
-              <React.Fragment>
-                <Row>
-                  <Col>
-                    {`${number}. ${question}`}
-                    <br />
-                  </Col>
-                </Row>
-                {test}
-                <br />
-              </React.Fragment>
-            )
-          })}
-          <Button onClick={() => setVisible(true)}>Add new question</Button>
-          {visible && (
-            <Modal isLarge onClose={() => setVisible(false)}>
-              <NewMultipleChoiceQuestion
-                onAddNewQuestion={onAddNewQuestion}
-                setVisible={setVisible}
-              />
-
-              <Close aria-label="Close modal" />
-            </Modal>
-          )}
-        </React.Fragment>
-      )
+    	return (
+	    	<AddMultipleChoiceRound 
+	    		questions={questions}
+	    		setQuestions={setQuestions}
+	    		answers={answers}
+	    		setAnswers={setAnswers}
+	    	/>
+			)      
     default:
       return <p>hi</p>
   }
 }
 
-const NewMultipleChoiceAnswer = ({
-  letter,
-  value,
-  onChangeAnswer,
-  radioValue,
-  setRadioValue,
-  correctAnswer,
-  setCorrectAnswer,
-}) => (
-  <Row justifyContent="center">
-    <Col>
-      <Input
-        value={value}
-        onChange={(e) => onChangeAnswer(letter, e.target.value)}
-      />
-      <Field>
-        <Radio
-          name="newQuestion"
-          value={letter}
-          checked={correctAnswer === letter}
-          onChange={(e) => {
-            setRadioValue(e.target.value)
-            setCorrectAnswer(e.target.value)
-          }}
-        >
-          <Label>{`${letter}.`}</Label>
-        </Radio>
-      </Field>
-    </Col>
-  </Row>
-)
-
-const NewMultipleChoiceQuestion = ({ onAddNewQuestion, setVisible }) => {
-  const [question, setQuestion] = useState('')
-  const [choices, setChoices] = useState({ a: '' })
-  const [correctAnswer, setCorrectAnswer] = useState('')
-  const [radioValue, setRadioValue] = useState('')
-
-  function nextCharacter(c) {
-    return String.fromCharCode(c.charCodeAt(0) + 1)
-  }
-
-  function onClickNewAnswer() {
-    const choiceKeys = Object.keys(choices)
-    const newKey = nextCharacter(choiceKeys[choiceKeys.length - 1])
-    const newChoices = { ...choices }
-    newChoices[newKey] = ''
-    setChoices(newChoices)
-  }
-
-  function onChangeAnswer(letter, value) {
-    const newChoices = { ...choices }
-    newChoices[letter] = value
-    setChoices(newChoices)
-  }
-
-  return (
-    <React.Fragment>
-      <Row justifyContent="center">
-        <Col sm={5}>
-          <Field>
-            <Label>Question:</Label>
-            <InputGroup>
-              <Input
-                value={question}
-                onChange={(e) => setQuestion(e.target.value)}
-              />
-            </InputGroup>
-          </Field>
-        </Col>
-      </Row>
-      <Row justifyContent="center">
-        <Col sm={5}>
-          <Field>
-            <Label>Answers:</Label>
-            {Object.entries(choices).map(([letter, value], i) => (
-              <NewMultipleChoiceAnswer
-                key={`answer-${i}`}
-                letter={letter}
-                value={value}
-                onChangeAnswer={onChangeAnswer}
-                radioValue={radioValue}
-                setRadioValue={setRadioValue}
-                correctAnswer={correctAnswer}
-                setCorrectAnswer={setCorrectAnswer}
-              />
-            ))}
-          </Field>
-        </Col>
-      </Row>
-      <br />
-      <Row justifyContent="center">
-        <Col sm={5}>
-          <Field>
-            <Button focusInset onClick={() => onClickNewAnswer()}>
-              Add another answer
-            </Button>
-          </Field>
-        </Col>
-      </Row>
-      <Row justifyContent="center">
-        <Col sm={5}>
-          <Field>
-            <Button
-              onClick={() => {
-                onAddNewQuestion(question, choices)
-                setVisible(false)
-              }}
-            >
-              Submit question
-            </Button>
-          </Field>
-        </Col>
-      </Row>
-      }
-    </React.Fragment>
-  )
-}
 
 const StyledButtons = styled.div`
   margin-top: ${(p) => p.theme.space.sm};
@@ -348,7 +155,7 @@ const RoundCreationStepper = ({ roundTypes }) => {
       ),
     },
     {
-      content: <DisplayQuestion q={questions.questions} />,
+      content: 'hi',
       buttons: <Button onClick={onBack}>Back</Button>,
     },
   ]
